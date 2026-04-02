3709dd671bbfa60e1e8cb564a93e266400909dab
Baymax1705 <yashverma.off17@gmail.com>
2026-04-03 00:19:28 +0530

    800_second

---
+++ 800/line_trip.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        vector<int>points;
+        points.push_back(0);
+        for(int i=0;i<n;i++)
+        {
+            int temp;
+            cin>>temp;
+            points.push_back(temp);
+        }
+        points.push_back(x);
+
+        int fuelCap=1;
+        n=points.size();                //n+2
+        for(int i=1;i<n;i++)
+        {
+            if(i==n-1)
+            {
+                fuelCap=max(fuelCap,2*(points[i]-points[i-1]));
+            }
+            else
+                fuelCap=max(fuelCap,points[i]-points[i-1]);
+        }
+        cout<<fuelCap<<endl;
+        // cout<<"----------"<<endl;
+    }
+
+    return 0;
+}
