6add2d6e0f6a47483cbdd5d043b85872bf9b964d
Baymax1705 <yashverma.off17@gmail.com>
2026-04-05 15:23:36 +0530

    800_third

--- 800/cover_water.cpp
+++ 800/cover_water.cpp
@@ -12,8 +12,15 @@ int main(){           //TC->O(N)            SC->O(1)
 
         bool flag_continous=false;     //flag for three continous dots
         int count_dots=0;
-        for(int i=1;i<n-1;i++)            //trimmed for edge case
+        for(int i=0;i<n;i++)            //trimmed for edge case
         {
+            if(i==0 || i==n-1)
+            {
+                if(s[i]=='.')
+                    count_dots++;
+                continue;
+            }
+            
             if(s[i]=='.')
             {
                 count_dots++;
@@ -24,11 +31,7 @@ int main(){           //TC->O(N)            SC->O(1)
                 }
             }
         }
-        if(s[0]=='.')
-            count_dots++;
-        if(s[n-1]=='.')
-            count_dots++;
-
+            
 
         if(flag_continous)
             cout<<"2"<<endl;
